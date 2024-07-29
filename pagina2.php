1ddc7990bae87bf29ba37e656b0021f8c3c23940
Luzaraque3 <luzaraque@colegioguanenta.edu.co>
2024-07-29 18:13:16 -0500

    Nuevo programa

---
+++ pagina2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>OPeradores PHP</title>
+    <meta charset="UTF-8">
+
+    <link rel= "incon" type="image/png" he>
+
+    
+</head>
+<body>
+    <h1>SISTEMAS</h1>
+    <h2>Guanentá</h2>
+    <hr>
+    <?php
+        $a = 10;
+        $b = 15; 
+        echo ("La suma de 10 y 15 es: ". ($a+$b)."<br/");
+        echo ("El residuo de 15 y 10 es: ". ($b%$a)."<br/");
+        echo ("La variable \$a con incremento: ". (++$a)."<br/");
+    ?>
+</body>
+</html>
